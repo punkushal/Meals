e64c9f4ab472365c8353f23042d8af974381117a
punkushal <spycymagar@gmail.com>
2023-09-24 14:02:19 +0545

    Created BluePrint Class For My Category Data

---
+++ lib/models/category.dart
@@ -0,0 +1,11 @@
+//Blueprint for my category data
+
+import 'package:flutter/material.dart';
+
+class Category {
+  final String id;
+  final String title;
+  final Color color;
+
+  Category({required this.id, required this.title, this.color = Colors.orange});
+}
