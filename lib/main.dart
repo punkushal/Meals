2f04f852fa3a6b88756b78ad79f497e511b88b9d
punkushal <spycymagar@gmail.com>
2023-09-24 14:06:09 +0545

    Screen VS Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meal/categories.dart';
+import 'package:meal/widgets/categories.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
