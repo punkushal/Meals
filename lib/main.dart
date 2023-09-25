97a8d6972d7b5ae4adaf3dbd528dfd70d71adf75
punkushal <spycymagar@gmail.com>
2023-09-25 13:58:37 +0545

    Again Added Categories Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meal/data/dummy_data.dart';
+
 import 'package:meal/screens/categories.dart';
-import 'package:meal/screens/meals.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -25,7 +24,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const MealsScreen(title: 'something..', meals: []),
+      home: const CategoriesScreen(),
     );
   }
 }
