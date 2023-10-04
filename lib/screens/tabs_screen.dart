1af59df86252155056828611ea131c64be91f45b
punkushal <spycymagar@gmail.com>
2023-10-04 17:51:31 +0545

    Added A Drawer

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:meal/models/meal.dart';
 import 'package:meal/screens/categories.dart';
 import 'package:meal/screens/meals.dart';
+import 'package:meal/widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -63,6 +64,7 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(activePageTitle),
       ),
+      drawer: const MainDrawer(),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
         onTap:
